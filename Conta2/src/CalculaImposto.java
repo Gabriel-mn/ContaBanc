8bcfdf7ab8271b9356f66464716363809ea1b350
Gabriel Martins Nascimento <112729735+Gabriel-mn@users.noreply.github.com>
2023-08-09 02:19:47 -0300

    TotalCode

---
+++ Conta2/src/CalculaImposto.java
@@ -0,0 +1,5 @@
+public interface CalculaImposto {
+    int  getCalculaImposto() ;
+
+
+}
---
+++ Conta2/src/br/Conta/modelo/CC.java
@@ -0,0 +1,27 @@
+package br.Conta.modelo;
+
+import excecao.Excecao;
+
+public class CC extends Conta{
+    private double taxa = 6.50;
+    public CC(int agencia, int numero) {
+        super(agencia, numero);
+    }
+
+    @Override
+    public void Saca(double valor) throws Excecao {
+        if(this.saldo <valor){
+            throw new Excecao("Valor invalido. . Primeiro faça um deposito para obter saque:");
+        }
+        else if (taxa+valor>this.saldo){
+            System.out.println("É necessário um valor de taxa");
+        }
+        else
+            System.out.println("Valor adicional de 6.00 para conta poupança");
+            System.out.println( this.saldo -=valor+taxa);
+
+    }
+
+    }
+
+
